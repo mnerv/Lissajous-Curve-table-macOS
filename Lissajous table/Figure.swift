07f0de6488b2c9c6520d0905aba7b3e4048cb99a
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2018-11-08 15:24:52 +0100

    Added create curves Added color to the curves

--- Lissajous table/Figure.swift
+++ Lissajous table/Figure.swift
@@ -13,21 +13,24 @@ class Rings {
     //MARK: Private Variables
     private let radius: CGFloat
     private var locationOrigin: CGPoint
-    private var circleColor: CGColor
     private let multipler: Double
     private var shape: SKShapeNode?
     private var dotShape: SKShapeNode?
     private var dotLocation: CGPoint?
     
     //MARK: Public Varables
+    var circleColor: CGColor
     var showTraceDot: Bool = true
     var traceDotRadius: Double = 5
+    var multipliedColor: CGColor?
     
     init(radius: CGFloat, locationOrigin: CGPoint, color: CGColor, multipler: Double) {
         self.radius = radius
         self.locationOrigin = locationOrigin
         self.multipler = multipler
         self.circleColor = color
+        
+        self.createCircle()
     }
     
     func createCircle() {
@@ -35,6 +38,10 @@ class Rings {
         self.shape?.position = self.locationOrigin
         self.shape?.strokeColor = NSColor(cgColor: self.circleColor)!
         createTraceDot(radius: traceDotRadius)
+    }
+    
+    func setMultipliedColor(colorA: CGColor, colorB: CGColor) {
+        self.multipliedColor = CGColor(red: colorA.components![0] * colorB.components![0], green: colorA.components![1] * colorB.components![1], blue: colorA.components![2] * colorB.components![2], alpha: colorA.components![3] * colorB.components![3])
     }
     
     func createTraceDot(radius: Double) {
@@ -63,6 +70,10 @@ class Rings {
         return self.dotShape!
     }
     
+    func getDotLocation() -> CGPoint {
+        return self.dotShape!.position
+    }
+    
     func getLocationOrigin() -> CGPoint {
         return self.locationOrigin
     }
@@ -71,10 +82,10 @@ class Rings {
 
 class Curve {
     private var shape: SKShapeNode?
-    private var dotShape: SKShapeNode
-    private var color: CGColor
+    var dotShape: SKShapeNode
     
     //MARK: Public Varables
+    var color: CGColor
     var showTraceDot: Bool = true
     var traceDotRadius: Double = 5
     var pointsArr = [CGPoint]()
@@ -83,12 +94,28 @@ class Curve {
         self.color = color
         self.dotShape = SKShapeNode(circleOfRadius: CGFloat(self.traceDotRadius))
         self.dotShape.position = startLocation
+        
+        self.createCurve()
+    }
+    
+    func createCurve() {
+        self.shape = SKShapeNode(points: &self.pointsArr, count: self.pointsArr.count)
+        self.shape?.lineWidth = 2
+        self.shape?.strokeColor = NSColor.init(cgColor: color)!
     }
     
     func appendPointsArray(location: CGPoint) {
         self.pointsArr.append(location)
         setTraceDotLocation(location: location)
-        
+    }
+    
+    func getDotShape() -> SKShapeNode {
+        return self.dotShape
+    }
+    
+    func getShape() -> SKShapeNode {
+        self.createCurve()
+        return self.shape!
     }
     
     func setTraceDotLocation(location: CGPoint) {
